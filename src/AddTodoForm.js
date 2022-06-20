a1e05f43b4904259cc9a055198815502300efdb0
neidacreates <neidaxmoreno@gmail.com>
2022-06-20 12:37:39 -0400

    finished lesson 1.3

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -6,7 +6,7 @@ const AddTodoForm = (props) => {
       let todoTitle = event.target.title.value;
       console.log(todoTitle);
       // confused on this part, the instructions say to pass newTodo as an argument but when I do that it doesn't work 
-      // props.onAddTodo(props.newTodo)
+    //   props.onAddTodo(props.newTodo)
       props.onAddTodo(todoTitle);
       event.target.reset();
     };
